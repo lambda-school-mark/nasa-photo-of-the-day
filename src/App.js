ad18245590e97e0b1b79cb4d3d1834a9da632473
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-06-10 18:29:57 -0400

    wow smh did I even commit?

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+
+const API_KEY = "7JOcMJeqEgWhnI6z4iSFMlcQqbJ8MPe3QoN3mtof";
+
+const Base_URL = "https://api.nasa.gov";
 
 function App() {
+  const [cardState, setCardState] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`${Base_URL}/planetary/apod?api_key=${API_KEY}`)
+      .then((Response) => {
+        console.log(Response);
+        setCardState(Response.data);
+      });
+  }, []);
+
+  const ApodCard = ({ info }) => (
+    <div className="card">
+      <div className="image-container">
+        <img alt="" src={info.hdurl}></img>
+        <p>{info.copyright}</p>
+      </div>
+      <div className="card-info">
+        <h1> {info.title}</h1>
+        <p> {info.explanation}</p>
+        <p> Date: {info.date}</p>
+      </div>
+    </div>
+  );
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <ApodCard info={cardState} />;
     </div>
   );
 }
